aaeea248e55b8c6232ba947d6265e3585262de46
Makoto Chiba <hypermkt@gmail.com>
2020-01-26 01:03:46 +0900

    Response by protocol buffers

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,19 @@
 class Api::V1::ItemsController < ApplicationController
   def index
     items = Item.all
-    render json: items
+    items_messages = items.map {|item| build_item_message(item)}
+
+    send_data(::ItemsResponse.encode(::ItemsResponse.new(
+      items: items_messages
+    )))
+  end
+
+  private
+
+  def build_item_message(item)
+    ::ItemResponse.new(
+      id: item.id,
+      title: item.title
+    )
   end
 end
--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -4,6 +4,12 @@ RSpec.describe 'Api::V1::Item', type: :request do
   describe '#index' do
     let!(:item) { create(:item) }
     before { get api_v1_items_url }
-    it { expect(response).to have_http_status(200) }
+    it 'response with protocol buffers' do
+      items_response = ::ItemsResponse.decode(response.body)
+      puts items_response
+      expect(response).to have_http_status(200)
+      expect(items_response.items[0].id).to eq item.id
+      expect(items_response.items[0].title).to eq item.title
+    end
   end
 end
