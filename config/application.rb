3b0a62b096f2e283ee0b405536b93926c5b32a35
Makoto Chiba <hypermkt@gmail.com>
2020-01-26 01:03:23 +0900

    Load protocol buffers files

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,7 @@ module RailsWithGrpc
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.autoload_paths += %W(#{config.root}/app/pb)
   end
 end
--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -14,3 +14,17 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym 'RESTful'
 # end
+
+class WithProtocolBufferInflector < Zeitwerk::Inflector
+  def camelize(basename, abspath)
+    if basename =~ /\A.*_pb$/
+      basename.gsub("_pb", '').camelize #<- `_pb` をここで除去した形でRails側でロードするように対応
+    else
+      super
+    end
+  end
+end
+
+Rails.autoloaders.each do |autoloader|
+  autoloader.inflector = WithProtocolBufferInflector.new
+end
