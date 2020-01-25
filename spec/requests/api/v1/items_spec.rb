becc26d66b044937b7179fc696a45f2089102059
Makoto Chiba <hypermkt@gmail.com>
2020-01-26 01:04:13 +0900

    Remove debug code

--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe 'Api::V1::Item', type: :request do
     before { get api_v1_items_url }
     it 'response with protocol buffers' do
       items_response = ::ItemsResponse.decode(response.body)
-      puts items_response
       expect(response).to have_http_status(200)
       expect(items_response.items[0].id).to eq item.id
       expect(items_response.items[0].title).to eq item.title
